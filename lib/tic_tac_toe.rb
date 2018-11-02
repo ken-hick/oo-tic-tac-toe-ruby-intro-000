9a1b764a662cdf4f15e2b17d2f9084428414df8c
Ken Hick <hick.ken@gmail.com>
2018-11-02 10:14:45 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,4 +20,15 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
+  WIN_COMBINATIONS = [
+    [0, 1, 2], # top row
+    [3, 4, 5], # middle row
+    [6, 7, 8], # bottom row
+    [0, 3, 6], # first columns
+    [1, 4, 7], # middle columns
+    [2, 5, 8], # third columns
+    [0, 4, 8], # diagonal left to right
+    [2, 4, 6] # diagonal right to left
+  ]
+
 end
