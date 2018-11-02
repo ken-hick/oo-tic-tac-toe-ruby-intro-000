d2e0c359379ca414cba37607b7df83ed84ff0c8e
Ken Hick <hick.ken@gmail.com>
2018-11-02 10:26:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,4 +59,16 @@ class TicTacToe
     end
   end
 
+  def won?
+    WIN_COMBINATIONS.each do |combo|
+      if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
+        return combo
+    elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+      return combo
+    end
+  end
+    return false
+  end
+
+
 end
